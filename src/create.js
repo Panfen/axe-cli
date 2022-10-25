7bea1a70af768b558d1c3458aa9b76bf9f118bcd
jifeng <jifeng.pjf@aistarfish.com>
2022-10-25 14:42:54 +0800

    first commit

---
+++ src/create.js
@@ -0,0 +1,77 @@
+/**
+ * 创建项目
+ */ 
+
+const axios = require('axios');
+const ora = require('ora');
+const inquirer  = require('inquirer');
+const { promisify } = require('util');
+const downloadGit = promisify(require('download-git-repo'))
+const ncp = promisify(require('ncp'))
+
+// 存放下载文件的目录：/Users/panjifei/.template
+const downloadDirectory = `${process.env[process.platform === 'darwin' ? 'HOME' : 'USERPROFILE']}/.template`;
+
+
+// 获取仓库列表
+const fetchRepoList = async () => {
+	const { data } = await axios.get('https://api.github.com/users/Panfen/repos');
+	return data;
+}
+
+// 获取版本信息
+const fetchTagList = async (repo) => {
+	const { data } = await axios.get(`https://api.github.com/repos/Panfen/${repo}/tags`);
+	return data;
+}
+
+// 下载项目
+const download = async (repo, tag) => {
+	let api = `Panfen/${repo}`;
+	if (tag) {
+		api += `#/${tag}`;
+	}
+	const dest = `${downloadDirectory}/${repo}`;
+	await downloadGit(api, dest);
+}
+
+// 封装fetch， 带loading 
+const wrapFetchAddLoading = (fn, message) => async (...args) => {
+	const spinner = ora(message);
+	spinner.start();
+	const res = await fn(...args);
+	spinner.succeed();
+	return res;
+}
+
+
+module.exports = async (programName) => {
+	let repos = await wrapFetchAddLoading(fetchRepoList, 'fetching template list...')();
+	repos = repos.map((repo) => repo.name)
+
+	// 选择模板
+	const { repo } = await inquirer.prompt({
+		name: 'repo',
+		type: 'list',
+		message: 'Please choose repo template to create project',
+		choices: repos
+	})
+
+	let tags = await wrapFetchAddLoading(fetchTagList, 'fetching template list...')(repo);
+	tags = tags.map((tag) => tag.name)
+
+	// 选择版本
+	const { tag } = await inquirer.prompt({
+		name: 'tag',
+		type: 'list',
+		message: 'Please choose tag template to create project',
+		choices: tags
+	})
+
+	// 下载项目
+	const target = await wrapFetchAddLoading(download, 'download template')(repo, tag)
+
+	// 拷贝项目到当前执行命令的目录下
+	await ncp(target, path.join(path.resolve(), repo))
+}
+
---
+++ src/main.js
@@ -0,0 +1,52 @@
+const program = require('commander')
+const path  = require('path')
+const { version } = require('./utils/constants');
+
+const actionsMap = {
+	create: {
+		description: 'create project',
+		alias: 'cp',
+		examples: [
+			'p-cli create <template-name>'
+		],
+	},
+	config: {
+		description: 'config info',
+		alias: 'ci',
+		examples: [
+			'p-cli config get <k>',
+			'p-cli config set <k> <v>'
+		],
+	},
+	'*': {
+		alias: 'none',
+		description: 'command not found'
+	}
+}
+
+// 循环创建命令
+Object.keys(actionsMap).forEach(action => {
+	program
+		.command(action) // 命令的名称
+		.alias(actionsMap[action].alias) // 命令的别名
+		.description(actionsMap[action].description) // 命令的描述
+		.action(() => { // 动作
+			if (action === '*') {
+				console.log(actionsMap[action].description)
+			} else {
+				require(path.resolve(__dirname, action))(...process.argv.slice(3))
+			}
+		})
+})
+
+program.on('--help', () => {
+	console.log('Examples:')
+	Object.keys(actionsMap).forEach((action) => {
+		(actionsMap[action].examples || []).forEach((example) => {
+			console.log(` ${example}`);
+		});
+	});
+});
+
+program.version(version)
+	.parse(process.argv) // // process.argv就是用户在命令行中传入的参数
